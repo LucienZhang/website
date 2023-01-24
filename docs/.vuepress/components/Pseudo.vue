ee20b1957fb8b9c91272be7ae16783efe3574603
Lucien Zhang <lucienzhangzl@outlook.com>
2023-01-24 18:27:17 +0900

    components

---
+++ docs/.vuepress/components/Pseudo.vue
@@ -0,0 +1,22 @@
+<template>
+  <pre ref="pseudo-content">
+    <slot></slot>
+  </pre>
+</template>
+
+<script>
+import pseudocode from "pseudocode";
+
+export default {
+  mounted() {
+    pseudocode.renderElement(this.$refs["pseudo-content"], {
+      indentSize: "1.5em",
+      lineNumber: true
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~pseudocode/build/pseudocode.min.css";
+</style>
