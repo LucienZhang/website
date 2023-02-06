803a2570815f5efd21eaff9c48703a4c35f8a90b
Lucien Zhang <lucienzhangzl@outlook.com>
2023-02-06 17:56:26 +0900

    fix pseudocode css

--- docs/.vuepress/components/Pseudo.vue
+++ docs/.vuepress/components/Pseudo.vue
@@ -20,6 +20,7 @@ if (!window.MathJax) {
 }
 await import("https://cdn.jsdelivr.net/npm/mathjax@3.0.0/es5/tex-chtml.js");
 await import("./assets/js/pseudocode");
+import("./assets/css/pseudocode.min.css");
 
 export default {
   mounted() {
@@ -30,7 +31,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-@import "./assets/css/pseudocode.min.css";
-</style>
